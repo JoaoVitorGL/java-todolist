96cb38df0483f63b25a0f47cb87af40e08ba26cd
JoaoVitorGl <vitorgouveia06@gmail.com>
2025-03-16 23:07:44 -0300

    Fix CORS

--- src/main/java/org/gouveia/to_do_list/ToDoListApplication.java
+++ src/main/java/org/gouveia/to_do_list/ToDoListApplication.java
@@ -1,12 +1,15 @@
 package org.gouveia.to_do_list;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class ToDoListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToDoListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListApplication.class, args);
+    }
 }
